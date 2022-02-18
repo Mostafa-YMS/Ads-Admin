a0feacac6eb478009a1bbb194f6938396f23a23a
Mostafa-YMS <mostafa_yms@yahoo.com>
2022-02-18 23:33:30 +0200

    close

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -3,6 +3,9 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createAd } from "../ads-redux/actions/creators";
 import { useNavigate } from "react-router-dom";
+import closeIcon from "../assets/imgs/icon-close.svg";
+import "../styles/forms.css";
+
 
 const CreatePage = () => {
   const [media, setMedia] = useState("");
@@ -39,8 +42,14 @@ const CreatePage = () => {
     }
     navigate("/");
   };
+  const close = () => {
+    navigate("/");
+  };
   return (
     <div className="card m-5">
+      <button onClick={close} className="close m-3 mb-0">
+        <img src={closeIcon} alt="X" className="close-icon" />
+      </button>
       <h2 className="m-3 fw-bold">Create</h2>
       <CreationForm
         media={media}
--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -4,6 +4,8 @@ import CreationForm from "../components/CreationForm";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { updateAd } from "../ads-redux/actions/creators";
+import closeIcon from "../assets/imgs/icon-close.svg";
+import "../styles/forms.css";
 
 const UpdatePage = () => {
   const dispatch = useDispatch();
@@ -64,8 +66,14 @@ const UpdatePage = () => {
     }
     navigate("/");
   };
+  const close = () => {
+    navigate("/");
+  };
   return (
     <div className="card m-5">
+      <button onClick={close} className="close m-3 mb-0">
+        <img src={closeIcon} alt="X" className="close-icon" />
+      </button>
       <h2 className="m-3 fw-bold">Update</h2>
       <CreationForm
         media={media}
